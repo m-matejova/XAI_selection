096329f136066fde5f405e6bca5f35b52e41f5fb
m-matejova <147806846+m-matejova@users.noreply.github.com>
2025-10-30 18:03:20 +0100

    Update XAI_SEL_APP.py

--- Application/XAI_SEL_APP.py
+++ Application/XAI_SEL_APP.py
@@ -8,35 +8,9 @@ pg = st.navigation([
 pg.run()
 
 
-st.set_page_config(  # Alternate names: setup_page, page, layout
-	layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
-	initial_sidebar_state="expanded",  # Can be "auto", "expanded", "collapsed"
-	page_title=None,  # String or None. Strings get appended with "• Streamlit".
-	page_icon=None,  # String, anything supported by st.image, or None.
+st.set_page_config( 
+	layout="wide",  
+	initial_sidebar_state="expanded",  
+	page_title=None,  
+	page_icon=None,  
 )
-
-
-
-
-
-#if st.button("Show graph for each method separately", icon=":material/graph_6:", key = "graphs"):
-
-from scipy.linalg import eigvals
-
-
-#st.title("Saaty's Pairwise Comparison Method")
-
-# Get criteria/alternatives from user
-
-
-#for i in range(0, weights22.shape[0]):
- #   st.write('w(g' + str(i + 1) + '): ', round(weights22[i], 3))
-
-# Consistency Ratio
-#st.write('RC: ' + str(round(rc, 2)))
-#if (rc > 0.10):
-#    st.write('The solution is inconsistent, the pairwise comparisons must be reviewed')
-#else:
-#    st.write('The solution is consistent')
-
-
